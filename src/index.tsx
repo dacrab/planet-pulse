5baded700eabc04f04d612e7184cc7159c829d2a
dacrab <vaggeliskavouras6@gmail.com>
2026-05-12 15:01:26 +0300

    feat: add @vercel/analytics and @vercel/speed-insights

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { render } from 'solid-js/web';
 import { MetaProvider } from '@solidjs/meta';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from './App';
 import './index.css';
+
+inject();
+injectSpeedInsights();
 
 const root = document.getElementById('root');
 
